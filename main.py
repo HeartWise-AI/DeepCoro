07d68bd122b1400ea7ecb1a897b6c9e361f18179
elodielabrecque <57810683+elodielabrecque@users.noreply.github.com>
2023-10-17 11:36:37 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,308 @@
+import argparse
+import pydicom
+import numpy as np
+import torch
+import pandas as pd
+import tensorflow as tf
+import keras_retinanet.models
+from keras_retinanet.utils.image import preprocess_image, resize_image
+import cv2
+import utils
+from tqdm import tqdm
+import imageio
+import scipy.ndimage
+import time
+
+def deepcoro(dicom_path, save_dir, models_dir, device):
+    dicom_info = pydicom.dcmread(dicom_path)
+    dicom = dicom_info.pixel_array
+    since = time.time()
+
+    try:
+        imager_spacing = float(dicom_info['ImagerPixelSpacing'][0])
+        factor = float(dicom_info['DistanceSourceToDetector'].value) / float(
+            dicom_info['DistanceSourceToPatient'].value)
+        pixel_spacing = float(imager_spacing / factor)
+        assert (imager_spacing > 0)
+        assert (factor > 0)
+    except:
+        assert(1 == 0), 'Pixel spacing information missing.'
+
+    try:
+        fps = int(dicom_info["RecommendedDisplayFrameRate"].value)
+    except:
+        assert(1 == 0), 'Frame rate information missing.'
+
+    assert(fps == 15), "Frame rate not equal to 15 FPS (" + str(fps) + " FPS)."
+
+    try:
+        StudyDate = str(dicom_info["StudyDate"].value)
+    except:
+        assert(1 == 0), "Study date information missing."
+
+    try:
+        DOB = str(dicom_info[(0x0010, 0x0030)].value)
+    except:
+        assert(1 == 0), "Patient age information missing."
+        
+    if len(models_dir) > 0:
+        if models_dir[-1] != "/":
+            models_dir = models_dir + "/"
+        
+    ###################
+    ### Algorithm 1 ###
+    ###################
+        
+    print("\nAlgorithm 1 / 6 starts")
+
+    since1 = time.time()
+
+    if 'cuda' in device:
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        if gpus:
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+    elif "cpu" in device:
+        tf.config.set_visible_devices([], 'GPU')
+
+    # 5: LCA, 9: RCA
+    object_model = tf.compat.v1.keras.models.load_model(models_dir + 'algorithm1.h5', compile=False)
+    out = []
+    for i in tqdm(range(dicom.shape[0])):
+        out1 = object_model(np.stack((dicom[i:i+1],) * 3, -1).astype(np.float32)/255)
+        out.append(out1[0])
+    object_pred = np.argmax(np.bincount(np.array(out).argmax(1)))
+
+    assert(object_pred in [5,9]), "DICOM does not display RCA or LCA."
+
+    del object_model
+
+    print("Algorithm 1 / 6 finished in", round(time.time() - since1, 2), 's')
+    
+    ###################
+    ### Algorithm 2 ###
+    ###################
+    
+    print("\nAlgorithm 2 / 6 starts")
+
+    since2 = time.time()
+
+    angle_model = tf.compat.v1.keras.models.load_model(models_dir + 'algorithm2a.h5', compile=False)
+    out = []
+    for i in tqdm(range(dicom.shape[0])):
+        out1 = angle_model(np.stack((dicom[i:i+1],) * 3, -1).astype(np.float32)/255)
+        out.append(out1[0])
+    angle_pred = np.argmax(np.bincount(np.array(out).argmax(1)))
+
+    if (object_pred == 9.0) & (angle_pred == 7.0):
+        angle_model = keras_retinanet.models.load_model(models_dir + 'algorithm2b/RCA_LAO_straight.h5', backbone_name="resnet50")
+    elif (object_pred == 5.0) & (angle_pred == 0.0):
+        angle_model = keras_retinanet.models.load_model(models_dir + 'algorithm2b/LAD_RAO_cranial.h5', backbone_name="resnet50")
+    else:
+        angle_model = keras_retinanet.models.load_model(models_dir + 'algorithm2b/general.h5', backbone_name="resnet50")
+
+    c = 0
+    stenoses = {}
+    for i in tqdm(range(dicom.shape[0])):
+        image = cv2.cvtColor(dicom[i].astype(np.uint8), cv2.COLOR_BGR2RGB)
+        image = preprocess_image(image)
+        image, scale = resize_image(image)
+
+        boxes, scores, labels = angle_model.predict_on_batch(np.expand_dims(image, axis=0))
+        boxes /= scale
+
+        idx_score = set(np.where(scores[0] >= 0.5)[0])
+        idx_labels = set(np.where(labels[0] == 15.0)[0])
+        idx = list(idx_score.intersection(idx_labels))
+
+        for j in idx:
+            stenoses[c] = {'frame': i, 'box': tuple(boxes[0, j])}
+            c += 1
+
+    del angle_model
+
+    print("Algorithm 2 / 6 finished in", round(time.time() - since2, 2), 's')
+    
+    ###################
+    ### Algorithm 3 ###
+    ###################
+    
+    print("\nAlgorithm 3 / 6 starts")
+
+    since3 = time.time()
+    imager_spacing = float(dicom_info['ImagerPixelSpacing'][0])
+    factor = float(dicom_info['DistanceSourceToDetector'].value) / float(
+        dicom_info['DistanceSourceToPatient'].value)
+    pixel_spacing = float(imager_spacing / factor)
+
+    for i in tqdm(stenoses.keys()):
+        frame = stenoses[i]['frame']
+        (x1, y1, x2, y2) = stenoses[i]['box']
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        x1n, y1n, x2n, y2n = utils.resize_coordinates(x1, y1, x2, y2, pixel_spacing, dicom.shape, 17.5)
+        stenoses[i]['box_resized'] = (x1n, y1n, x2n, y2n) 
+        stenoses[i]['reg_shift'] = utils.register(dicom, frame, x1n, y1n, x2n, y2n)
+
+    print("Algorithm 3 / 6 finished in", round(time.time() - since3, 2), 's')
+    
+    ###################
+    ### Algorithm 4 ###
+    ###################
+    
+    print("\nAlgorithm 4 / 6 starts")
+
+    since4 = time.time()
+    model_paths = [
+        models_dir + "algorithm4/bs_64_lr_0.00107809_model_FPN_loss_LovaszLoss.pth",
+        models_dir + "algorithm4/bs_64_lr_0.00242160_model_DeepLabV3Plus_loss_LovaszLoss.pth",
+        models_dir + "algorithm4/bs_64_lr_0.00129894_model_PAN_loss_TverskyLoss.pth",
+        models_dir + "algorithm4/bs_4_lr_0.00059902_model_DeepLabV3_loss_TverskyLoss.pth",
+        models_dir + "algorithm4/bs_16_lr_0.00993245_model_FPN_loss_LovaszLoss.pth",
+        models_dir + "algorithm4/bs_16_lr_0.00232125_model_DeepLabV3_loss_LovaszLoss.pth",
+        models_dir + "algorithm4/bs_64_lr_0.00646546_model_PAN_loss_DiceLoss.pth"
+    ]
+
+    models = [utils.load_model(path, device) for path in model_paths]
+
+    output = np.zeros(dicom.shape)
+    for i in tqdm(range(dicom.shape[0])):
+        output[i] = utils.perform_segmentation_inference(models, dicom[i], device)
+
+    del models
+    if 'cuda' in device:
+        torch.cuda.empty_cache()
+
+    print("Algorithm 4 / 6 finished in", round(time.time() - since4, 2), 's')
+    
+    ###################
+    ### Algorithm 5 ###
+    ###################
+    
+    print("\nAlgorithm 5 / 6 starts")
+
+    since5 = time.time()
+    for i in tqdm(range(len(stenoses))):
+        frame = stenoses[i]['frame']
+        (x1, y1, x2, y2) = stenoses[i]['box_resized']
+
+        preds = []
+        pad_value = max([(x2 - x1), (y2 - y1)])
+        for j in range(output.shape[0]):
+            reg_shift = stenoses[i]['reg_shift'][j]
+            padded_output = np.pad(output[j], ((pad_value, pad_value), (pad_value, pad_value)), mode='constant', constant_values=0)
+
+            y_shift = -reg_shift[0] + pad_value
+            x_shift = -reg_shift[1] + pad_value
+            region = padded_output[int(y1 + y_shift): int(y2 + y_shift), int(x1 + x_shift): int(x2 + x_shift)].astype('uint8')
+
+            segment = utils.get_segment_center(region, int(object_pred))
+            preds.append(segment)
+
+        cleaned_preds = [i for i in preds if not(i in ['None', 'other'])]
+        if len(cleaned_preds) == 0:
+            pred_segment = 'None'
+        else:
+            pred_segment = max(set(cleaned_preds), key=cleaned_preds.count)
+
+        stenoses[i]['artery_segment'] = pred_segment
+
+    def get_middle(group):
+        return group.iloc[int(len(group) / 2)]
+
+    df_stenoses = pd.DataFrame(stenoses).T
+    df_stenoses = df_stenoses.loc[df_stenoses['artery_segment'] != 'None'].reset_index(drop=True)
+    df_stenoses = df_stenoses.groupby(['artery_segment']).apply(get_middle).reset_index(drop=True)
+
+    print("Algorithm 5 / 6 finished in", round(time.time() - since5, 2), 's')
+    
+    ###################
+    ### Algorithm 6 ###
+    ###################
+    
+    print("\nAlgorithm 6 / 6 starts")
+
+    since6 = time.time()
+    artery_labels = {'leftmain': 0, 'lad': 1, 'lcx': 2, 'mid_lad': 3, 'dist_lcx': 4, 'dist_lad': 5, 'prox_rca': 6, 'mid_rca': 7, 'dist_rca': 8, 'pda': 9, 'posterolateral': 10}
+    THRESH = 0.23
+
+    model = utils.get_model()
+    model = model.to(device).double()
+    checkpoint = torch.load(models_dir + 'algorithm6.pt', map_location=device)["state_dict"]
+    if device == 'cpu':
+        checkpoint = {key.replace('module.', ''): value for key, value in checkpoint.items()}
+    else:
+        model = torch.nn.DataParallel(model)
+    model.eval()
+    model.load_state_dict(checkpoint)
+
+    patient_age = utils.get_age(dicom_info)
+
+    df_stenoses['percent_stenosis'] = None
+    df_stenoses['severe_stenosis'] = None
+    for i in tqdm(range(len(df_stenoses))):
+        (x1, y1, x2, y2) = df_stenoses['box_resized'].iloc[i]
+        reg_shifts = df_stenoses['reg_shift'].iloc[i]
+        frame = df_stenoses['frame'].iloc[i]
+        video = utils.create_cropped_registered_video(dicom, frame, reg_shifts, x1, y1, x2, y2)
+        age = torch.tensor([[patient_age]])
+        segment = torch.tensor([[artery_labels[df_stenoses['artery_segment'].iloc[i]]]])
+        outputs = model([video.to(device), age.to(device), segment.to(device)])
+
+        df_stenoses.loc[i, 'percent_stenosis'] = outputs.item() * 100
+        df_stenoses.loc[i, 'severe_stenosis'] = (outputs.item() >= THRESH)
+
+    print("Algorithm 6 / 6 finished in", round(time.time() - since6, 2), 's')
+    
+    ####################
+    ### Save outputs ###
+    ####################
+
+    print("\nSaving output...")
+    if len(save_dir) > 0:
+        if save_dir[-1] != "/":
+            save_dir = save_dir + "/"
+
+    df_stenoses['video_path'] = None
+    font = cv2.FONT_HERSHEY_PLAIN
+    for i in tqdm(range(len(df_stenoses))):
+        if df_stenoses['severe_stenosis'].iloc[i]:
+            save_path = 'severe_stenosis.mp4'
+        else:
+            save_path = 'nonsevere_stenosis.mp4'
+        save_path = save_dir + df_stenoses['artery_segment'].iloc[i] + "_" + str(int(df_stenoses['percent_stenosis'].iloc[i])) + "_" + save_path
+        df_stenoses.loc[i, 'video_path'] = save_path
+
+        (x1, y1, x2, y2) = df_stenoses['box_resized'].iloc[i]
+        img_reg = np.zeros(dicom.shape)
+        for j in range(dicom.shape[0]):
+            reg_shift = df_stenoses['reg_shift'].iloc[i][j]
+            img_reg[j] = scipy.ndimage.shift(dicom[j], shift=(reg_shift[0], reg_shift[1]), order=5)
+            cv2.rectangle(img_reg[j], (x1, y1), (x2, y2), (255, 255, 255), 1)
+            cv2.putText(img_reg[j], df_stenoses['artery_segment'].iloc[i] + " - " + str(int(df_stenoses['percent_stenosis'].iloc[i])) + '%', org=(x1, y1), fontFace=font, fontScale=1,color=(255, 255, 255))
+        imageio.mimwrite(save_path, img_reg.astype(np.uint8), fps=15)
+
+    df_stenoses[['video_path', 'frame', 'box_resized', 'artery_segment', 'percent_stenosis', 'severe_stenosis']].to_csv(save_dir + "df_stenosis.csv")
+
+    total_time = int(time.time() - since)
+    print("Total running time:", int(total_time // 60), 'min', int(total_time % 60), "s")
+
+
+def main(args = None):
+    parser = argparse.ArgumentParser(description = 'DeepCoro')
+    
+    parser.add_argument('--dicom_path')
+    parser.add_argument('--save_dir')
+    parser.add_argument('--models_dir', default = 'models/')
+    parser.add_argument('--device', default = 'cuda')
+    parser = parser.parse_args(args)
+    
+    dicom_path = parser.dicom_path
+    save_dir = parser.save_dir
+    models_dir = parser.models_dir
+    device = parser.device
+ 
+    deepcoro(dicom_path, save_dir, models_dir, device)
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,309 @@
+import numpy as np
+from scipy.ndimage import uniform_filter1d
+import cv2
+import torch
+import torchvision
+import segmentation_models_pytorch
+import scipy.ndimage
+import torch.nn as nn
+import torchvision.models.video
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+
+###################
+### Algorithm 3 ###
+###################
+
+def resize_coordinates(x1, y1, x2, y2, pixel_spacing, img_shape, target_size):
+    xc = np.floor((x2 + x1) / 2)
+    yc = np.floor((y2 + y1) / 2)
+
+    nPixel = target_size / pixel_spacing
+
+    (x1n, y1n, x2n, y2n) = (
+        int(np.floor(xc - nPixel / 2)),
+        int(np.floor(yc - nPixel / 2)),
+        int(np.floor(xc + nPixel / 2)),
+        int(np.floor(yc + nPixel / 2))
+    )
+
+    if x1n < 0:
+        x2n -= x1n
+        x1n = 0
+    if x2n > img_shape[1]:
+        if x1n != 0:
+            x1n -= x2n - img_shape[1]
+        x2n = img_shape[1]
+    if y1n < 0:
+        y2n -= y1n
+        y1n = 0
+    if y2n > img_shape[2]:
+        if y1n != 0:
+            y1n -= y2n - img_shape[2]
+        y2n = img_shape[2]
+
+    assert abs(y2n - y1n) == abs(x2n - x1n), "Resized region is not square."
+
+    return x1n, y1n, x2n, y2n
+
+def register(img, index, x1, y1, x2, y2):
+    img_after = img[index:]
+    img_before = np.flip(
+        img[: index + 1], 0
+    )  
+    plx = []
+    ply = []
+    k = 0
+    tracker = cv2.TrackerCSRT.create() 
+
+    tracker.init(img_before[0], [x1, y1, x2 - x1, y2 - y1])
+    for i in range(img_before.shape[0]):
+        moving = img_before[i]
+        _, bbox = tracker.update(moving)
+        (x, y, w, h) = (int(v) for v in bbox)
+
+        yoff = int(y + h / 2 - (y2 - y1) / 2) - y1
+        xoff = int(x + w / 2 - (x2 - x1) / 2) - x1
+
+        plx.append(xoff)
+        ply.append(yoff)
+
+    plx.reverse()
+    ply.reverse()
+
+    plx.pop()
+    ply.pop()
+
+    tracker.init(img_after[0], [x1, y1, x2 - x1, y2 - y1])
+    for i in range(img_after.shape[0]):
+        moving = img_after[i]
+        _, bbox = tracker.update(moving)
+        (x, y, w, h) = (int(v) for v in bbox)
+
+        yoff = int(y + h / 2 - (y2 - y1) / 2) - y1
+        xoff = int(x + w / 2 - (x2 - x1) / 2) - x1
+
+        plx.append(xoff)
+        ply.append(yoff)
+
+    plxo1 = uniform_filter1d(plx, size=5, output=float)
+    plyo1 = uniform_filter1d(ply, size=5, output=float)
+
+    reg_shift = {}
+    for i in range(img.shape[0]):
+        reg_shift[i] = [-plyo1[i], -plxo1[i]]
+
+    return reg_shift
+
+###################
+### Algorithm 4 ###
+###################
+
+def choose_model(model_name):
+    if model_name == 'Unet':
+        return segmentation_models_pytorch.Unet(encoder_weights="imagenet", in_channels=3, classes=26)
+    elif model_name == 'UnetPlusPlus':
+        return segmentation_models_pytorch.UnetPlusPlus(encoder_weights="imagenet", in_channels=3, classes=26)
+    elif model_name == 'MAnet':
+        return segmentation_models_pytorch.MAnet(encoder_weights="imagenet", in_channels=3, classes=26)
+    elif model_name == 'Linknet':
+        return segmentation_models_pytorch.Linknet(encoder_weights="imagenet", in_channels=3, classes=26)
+    elif model_name == 'FPN':
+        return segmentation_models_pytorch.FPN(encoder_weights="imagenet", in_channels=3, classes=26)
+    elif model_name == 'PSPNet':
+        return segmentation_models_pytorch.PSPNet(encoder_weights="imagenet", in_channels=3, classes=26)
+    elif model_name == 'DeepLabV3':
+        return segmentation_models_pytorch.DeepLabV3(encoder_weights="imagenet", in_channels=3, classes=26)
+    elif model_name == 'DeepLabV3Plus':
+        return segmentation_models_pytorch.DeepLabV3Plus(encoder_weights="imagenet", in_channels=3, classes=26)
+    elif model_name == 'PAN':
+        return segmentation_models_pytorch.PAN(encoder_weights="imagenet", in_channels=3, classes=26)
+    else:
+        raise ValueError(f"Invalid model name: {model_name}. Please choose a valid model.")
+
+def load_model(checkpoint_path, device):
+    split_parts = checkpoint_path.split('_')
+    model_index = split_parts.index("model")
+    model_name = split_parts[model_index+1]
+    model = choose_model(model_name)
+
+    checkpoint = torch.load(checkpoint_path, map_location = device)
+    model.load_state_dict(checkpoint['state_dict'])
+    model.to(device)
+    model.eval()
+    return model
+
+def histogram_equalization(image):
+    image_pil = torchvision.transforms.ToPILImage()(image)
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    cl1 = clahe.apply(np.asarray(image_pil))
+    return torch.from_numpy(cl1).unsqueeze(0)
+
+def process_image(img):
+    assert(len(img.shape) == 2)
+    image = torch.tensor(img).float().unsqueeze(0)
+    image = image.to(torch.uint8)
+    image = histogram_equalization(image)
+    image = (image - image.min()) / (image.max() - image.min())
+    return image
+
+def perform_segmentation_inference(models, img, device):
+    image = process_image(img).repeat(1, 3, 1, 1).to(device)
+    with torch.no_grad():
+        outputs = [model(image) for model in models]
+    combined_output = torch.stack(outputs).mean(dim=0).argmax(1).squeeze().cpu().numpy()
+    return combined_output
+
+###################
+### Algorithm 5 ###
+###################
+
+retinanet_artery_labels = { # https://syntaxscore.org/index.php/tutorial/definitions
+    1: 'prox_rca', # 1: RCA proximal
+    2: 'mid_rca', # 2: RCA mid 
+    3: 'dist_rca', # 3: RCA distal
+    4: 'pda', # 4: Posterior descending
+    5: 'leftmain', # 5: Left main 
+    6: 'lad', # 6: LAD proximal 
+    7: 'mid_lad', # 7: LAD mid 
+    8: 'dist_lad', # 8: LAD apical
+    9: 'other', # 9: First diagonal 
+    10: 'other', # 9a: First diagonal a
+    11: 'other', # 10: Second diagonal
+    12: 'other', # 10a: Second diagonal a
+    13: 'lcx', # 11: Proximal circumflex 
+    14: 'other', # 12: Intermediate/anterolateral
+    15: 'other', # 12a: Obtuse marginal a 
+    16: 'dist_lcx', # 13: Distal circumflex 
+    17: 'other', # 14: Left posterolateral 
+    18: 'other', # 14a: Left posterolateral a 
+    19: 'other', # 15: Posterior descending
+    20: 'posterolateral', # 16: Posterolateral from RCA
+    21: 'posterolateral', # 16a: Posterolateral from RCA 
+    22: 'posterolateral', # 16b: Posterolateral from RCA
+    23: 'posterolateral', # 16c: Posterolateral from RCA
+    24: 'other', # 12b: Obtuse marginal b
+    25: 'other', # 14b: Left posterolateral b
+    26: 'other' # stenosis
+}
+
+which_artery = {9: [1,2,3,4,20,21,22,23],
+                5: [5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,24,25]}
+
+def get_segment_region(region, object_value):
+    unique, counts = np.unique(region, return_counts=True)
+    d = dict(zip(unique, counts))
+    d1 = dict(sorted(d.items(), key=lambda item: item[1], reverse = True))
+    if 0 in d1.keys():
+        del d1[0]
+    segment = 'None'
+    for value in d1.keys():
+        if (value in which_artery[object_value]):
+            segment = retinanet_artery_labels[value]
+            break
+    return segment
+
+def get_segment_center(region, object_value):
+    center = [int(region.shape[0] / 2), int(region.shape[0] / 2)]
+    min_L = min(center)
+    d1 = {"None": "region2"}
+    for l in range(min_L):
+        if l != 0:
+            subregion = region[center[0] - l: center[0] + l, center[1] - l: center[1] + l]
+        else:
+            subregion = region[center[0]: center[0] + 1, center[1]: center[1] + 1]
+        if subregion.sum() == 0:
+            segment = 'None'
+        else:
+            segment = get_segment_region(subregion, object_value)
+        if not(segment in ['None']):
+            break
+    return segment
+
+###################
+### Algorithm 6 ###
+###################
+
+def create_cropped_registered_video(dicom, frame, reg_shifts, x1, y1, x2, y2):
+    FRAMES = 24
+    RESIZE = 224
+    mean = [144.80116, 144.80116, 144.80116] 
+    std = [52.669548, 52.669548, 52.669548]
+
+    cropped_img_reg = np.zeros(dicom[:, y1:y2, x1:x2].shape)
+    for j in range(dicom.shape[0]):
+        reg_shift = reg_shifts[j]
+        img_reg = scipy.ndimage.shift(dicom[j], shift=(reg_shift[0], reg_shift[1]), order=5)
+        cropped_img_reg[j] = img_reg[y1:y2, x1:x2]
+        
+    cropped_img_reg = np.stack((cropped_img_reg,) * 3, 1)
+    cropped_img_reg = torch.from_numpy(cropped_img_reg)
+    cropped_img_reg = torchvision.transforms.Resize((RESIZE, RESIZE))(cropped_img_reg)
+    cropped_img_reg = torchvision.transforms.Normalize(mean, std)(cropped_img_reg).numpy()
+    
+    pad_value = int(FRAMES / 2)
+    zero_frame = np.zeros(cropped_img_reg[0:1].shape)
+    padded_video = np.concatenate([zero_frame] * pad_value + [cropped_img_reg] + [zero_frame] * pad_value, axis=0)
+    pad_frame = frame + pad_value
+    final_video = padded_video[pad_frame - pad_value: pad_frame + pad_value]
+    final_video = np.stack((final_video,) * 1, 0).transpose((0, 2, 1, 3, 4))
+    
+    return torch.from_numpy(final_video)
+
+class Identity(nn.Module):
+    def __init__(self):
+        super(Identity, self).__init__()
+        
+    def forward(self, x):
+        return x
+    
+class Regressor(nn.Module):
+    def __init__(self, num_class = 1, num_features = 12):
+        super(Regressor, self).__init__()
+        
+        
+        model = torchvision.models.video.swin3d_b(weights='KINETICS400_IMAGENET22K_V1')
+        n_inputs = model.head.in_features
+        model.head = nn.Linear(n_inputs, num_class)
+        #model = nn.Sequential(model, nn.Sigmoid())
+
+        #model = model[0]
+        n_inputs = model.head.in_features
+        model.head = Identity()
+        
+        self.model = model
+        self.fc = nn.Linear(n_inputs + num_features, num_class)
+        self.layer = nn.Sigmoid()
+
+    def forward(self, x):
+        features = self.model(x[0])
+        
+        age = x[1]/120
+        
+        segment = torch.nn.functional.one_hot(x[2].squeeze(), num_classes=11).to(torch.float32)
+        if len(segment.size()) == 1:
+            segment = segment.unsqueeze(0)
+        x = torch.cat([features, segment, age], 1)
+
+        x = self.fc(x)
+        x = self.layer(x)
+        return x
+
+def get_model():
+    model = Regressor()
+    return model
+
+def get_age(dicom_info):
+    StudyDate = str(dicom_info["StudyDate"].value)
+    DOB = str(dicom_info[(0x0010, 0x0030)].value)
+    
+    [ySD, mSD, dSD] = [int(StudyDate[0:4]), int(StudyDate[4:6]), int(StudyDate[6:8])]
+    [yDOB, mDOB, dDOB] = [int(DOB[0:4]), int(DOB[4:6]), int(DOB[6:8])]
+    d1 = date(yDOB,mDOB,dDOB)
+    d2 = date(ySD,mSD,dSD)
+    age = relativedelta(d2, d1).years
+    
+    return age
+
+
